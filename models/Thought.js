f693070c5fac24e799d299a4df13b76ec03fa7ec
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-22 19:25:40 -0700

    added base files, working on modal structures

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const { Thought } = require('.');
+const dateFormat = require("../utils/dateFormat");
+
+
+const thoughtSchema = new Schema( {
+    thoughtText: {
+        type: String,
+        required: true,
+        max_length: 128,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createAtVal) => dateFormat(createAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+}
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: { 
+            type: String,
+            required: true,
+            match: /.+\@.+\..+/,
+            unique: true,
+        },
+        thoughts: {
+            //TODO
+        },
+        friends: {
+            //TODO
+        },
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+    }
+
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
