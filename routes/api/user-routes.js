e13c628e4d24f561ef38bc62b2b8d2531cbc8f0f
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-28 22:47:58 -0700

    server.js running

---
+++ routes/api/user-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const mongoose = require('mongoose')
+
+const { User, Thought, } = require('../../models');
+
+
+router.get('/', (req, res) => {
+    
+})
